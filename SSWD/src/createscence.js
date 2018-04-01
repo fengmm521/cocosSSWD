9d3530dc2604ca90f9c44b0d93a901fe435e8615
woodcol <fengmm521@gmail.com>
2018-04-02 01:37:42 +0800

    button event

--- SSWD/src/createscence.js
+++ SSWD/src/createscence.js
@@ -4,25 +4,15 @@ var creaesLayer = cc.Layer.extend({
         this._super();
         var node,
             file = res.createRoom;
-        if(cocoStudioOldApiFlag == 0){
-            cc.log("ccs.load : %s", file);
-            var json = ccs.load(file);
-            node = json.node;
-        }else{
-            //ccs.sceneReader only supports 1.x file
-            cc.log("ccs.sceneReader.createNodeWithSceneFile : %s", file);
-            node = ccs.sceneReader.createNodeWithSceneFile(file);
-        }
+        var json = ccs.load(file);
+        node = json.node;
         this.addChild(node);
         // ccs.actionManager.playActionByName("startMenu_1.json", "Animation1");
-        this.initSize(node);
+        // this.initSize(node);
     },
     onExit: function() {
         ccs.actionManager.releaseActions();
         this._super();
-    },
-    title: function () {
-        return "loadSceneEdtiorFile Test";
     }
 });
 
--- SSWD/src/mainscence.js
+++ SSWD/src/mainscence.js
@@ -5,37 +5,89 @@ var FirstUILayer = cc.Layer.extend({
         //////////////////////////////
         // 1. super init first
         this._super();
-
-        /////////////////////////////
-        // 2. add a menu item with "X" image, which is clicked to quit the program
-        //    you may modify it.
-        // ask the window size
-        var size = cc.winSize;
-
-        /////////////////////////////
-        // 3. add your codes below...
-        // add a label shows "Hello World"
-        // create and initialize a label
-        var helloLabel = new cc.LabelTTF("Hello World", "Arial", 38);
-        // position the label on the center of the screen
-        helloLabel.x = size.width / 2;
-        helloLabel.y = size.height / 2 + 200;
-        // add the label as a child to this layer
-        this.addChild(helloLabel, 5);
-
-        // add "HelloWorld" splash screen"
-        this.sprite = new cc.Sprite(res.HelloWorld_png);
-        this.sprite.attr({
-            x: size.width / 2,
-            y: size.height / 2
-        });
-        this.addChild(this.sprite, 0);
+        
 
         return true;
+    },
+
+    onEnter: function () {
+        this._super();
+
+        var node,
+            file = res.mainScene;
+        var json = ccs.load(file);
+        node = json.node;
+        this.addChild(node);
+
+        
+        // 创建游戏
+        var cBtn = node.getChildByName("btncreateroom");
+        cBtn.addTouchEventListener(this.touchEventCBtn, this);
+        // #/Volumes/mage/applications/Cocos/Cocos2d-x/cocos2d-x-3.15
+
+        var oBtn = node.getChildByName("inbtn");
+        oBtn.addTouchEventListener(this.touchEventinBtn, this);
+
+        // ccs.actionManager.playActionByName("startMenu_1.json", "Animation1");
+        // this.initSize(node);
+    },
+    onExit: function() {
+        ccs.actionManager.releaseActions();
+        this._super();
+    },
+    touchEventCBtn:function(sender, type){
+        switch (type) {
+            case ccui.Widget.TOUCH_BEGAN:
+                    cc.log("touch began");
+                    sender.setScale(0.95);
+                break;
+
+            case ccui.Widget.TOUCH_MOVED:
+//                cc.log("touch move");
+                break;
+
+            case ccui.Widget.TOUCH_ENDED:
+                cc.log("touch end");
+                    sender.setScale(1.0);
+                break;
+
+            case ccui.Widget.TOUCH_CANCELED:
+                cc.log("touch cancele");
+                    sender.setScale(1.0);
+                break;
+
+            default:
+                break;
+        }
+    },
+    touchEventinBtn:function(sender, type){
+        switch (type) {
+            case ccui.Widget.TOUCH_BEGAN:
+                    cc.log("touch began");
+                    sender.setScale(0.95);
+                break;
+
+            case ccui.Widget.TOUCH_MOVED:
+//                cc.log("touch move");
+                break;
+
+            case ccui.Widget.TOUCH_ENDED:
+                cc.log("touch end");
+                    sender.setScale(1.0);
+                break;
+
+            case ccui.Widget.TOUCH_CANCELED:
+                cc.log("touch cancele");
+                    sender.setScale(1.0);
+                break;
+
+            default:
+                break;
+        }
     }
 });
 
-var FirstUILayer = cc.Scene.extend({
+var FirstUIScence = cc.Scene.extend({
     onEnter:function () {
         this._super();
         var layer = new FirstUILayer();
